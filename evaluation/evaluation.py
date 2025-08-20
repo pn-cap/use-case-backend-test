580b88819c0a0f14298bd9570578b8b07f042a35
Patryk Nowicki <patryk.nowicki@capgemini.com>
2025-08-20 14:20:11 +0200

    Update evaluation evaluation.py

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -1,6 +1,6 @@
 # evaluation/evaluation.py
 with open("results.md", "w") as f:
     f.write("## All checks passed!\n  ")
-    f.write("test 1")
-    f.write("test 2")
+    f.write("# test 1  \n ")
+    f.write("# test 2  \n ")
 
